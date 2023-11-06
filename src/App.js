757430ba04481086847506f7ed2eacb320dda16e
Thu Trang Vu <vuthutrang2811@gmail.com>
2023-11-06 19:22:31 +0200

    Lopputyön osa 1 valmis

---
+++ src/App.js
@@ -0,0 +1,154 @@
+
+
+import * as React from 'react';
+import {useState} from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Customerlist from './components/Customerlist'
+import Traininglist from './components/Traininglist'
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function App() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [navBarComponent, setNavBarComponent] = useState(null);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleDrawerItemSelected = (text) =>{
+    if(text ==="Asiakkaat"){
+      setNavBarComponent(<Customerlist />)
+    }
+    if (text ==="Harjoitukset"){
+      setNavBarComponent(<Traininglist />)
+    }
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Personal Trainer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Asiakkaat', 'Harjoitukset'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick = {() => handleDrawerItemSelected(text)}>
+                <ListItemIcon>
+                {index === 1 ? <DirectionsRunIcon /> : <AccountBoxIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        {navBarComponent ||
+        (<Typography paragraph>
+          <h1>Tervetuloa Personal trainer-yritykseemme.</h1>
+        </Typography>)}
+      </Main>
+    </Box>
+  );
+}
---
+++ src/components/Traininglist.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect, useMemo, useRef} from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import dayjs from 'dayjs';
+
+export default function Traininglist(){
+
+    const [trainings, setTrainings] = useState([]);
+    useEffect(() => fetchData(), []);
+    const fetchData = () => {
+        fetch('https://traineeapp.azurewebsites.net/gettrainings')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => setTrainings(data))
+          .catch(error => console.error('Error fetching data:', error));
+      };
+    
+      const dateFormatter = (params) => {
+        const formattedDate = dayjs(params.value).format('DD.MM.YYYY');
+        return formattedDate;
+        };
+        const timeFormatter = (params) => {
+            const formattedTime = dayjs(params.value).format('HH:mm');
+            return formattedTime;
+        };
+    
+    const columns = [
+        {field : "date" , headerName : "Pvm", sortable : true, filter : true, cellRenderer: dateFormatter, width : 150},
+        {field : "date" , headerName : "Aika", sortable : true, filter : true, cellRenderer: timeFormatter, width: 150},
+        {field : "duration" , headerName : "Kesto(min)", sortable : true, filter : true, width : 150},
+        {field : "activity" , headerName : "Harjoitus", sortable : true, filter : true, width : 200},
+        {field: 'customer.firstname', headerName: 'Asiakas', sortable: true, filter: true, width : 200,
+        valueGetter: 
+            (params) => {
+                return params.data.customer.firstname + " " + params.data.customer.lastname;
+            },
+        },
+        {headerName: ""}
+    ]
+
+    const pagination = true;
+    const paginationPageSize = 10;
+    const gridStyle = useMemo(() => ({ height: '550px', width: '70%' }), []);
+    return (
+        <div>
+            <h1> Harjoitusten lista</h1>
+            <div style={gridStyle} className="ag-theme-alpine">
+                <AgGridReact 
+                    pagination={pagination}
+                    paginationPageSize={paginationPageSize}
+                    rowData={trainings} 
+                    columnDefs={columns} />
+            </div>   
+      </div>
+    )
+}
